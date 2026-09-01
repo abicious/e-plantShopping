c07cbf0bd89ecac1739a53b5b735c72f12c801cf
abicious <drabicious@gmail.com>
2026-09-02 02:50:03 +0500

    Create App component for landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Paradise Nursery Landing Page</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
