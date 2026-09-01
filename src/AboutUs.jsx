e0b28c7d1be13e61117d32de8790b9183495208d
abicious <drabicious@gmail.com>
2026-09-02 02:47:21 +0500

    Add AboutUs component with introductory content

---
+++ src/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>Welcome to Paradise Nursery, where green meets serenity. We bring nature closer to your home.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
