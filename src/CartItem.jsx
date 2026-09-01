8f88dc1e53e2ba8525f7f9b7109b2b631738f7ec
abicious <drabicious@gmail.com>
2026-09-02 02:51:18 +0500

    Add ProductList component for houseplants category

---
+++ src/CartItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductList() {
+  return (
+    <div className="product-list">
+      <h2>Houseplants Category</h2>
+      <p>Add to Cart: <button>Add to Cart</button></p>
+    </div>
+  );
+}
+
+export default ProductList;
