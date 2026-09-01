68cadaa415a1a97ff815ae26b53848f16e6a8a13
abicious <drabicious@gmail.com>
2026-09-02 02:54:17 +0500

    Refactor CartItem component with Redux integration

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,12 +1,70 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem, updateQuantity } from './CartSlice';
+import './CartItem.css';
 
-function ProductList() {
+const CartItem = ({ onContinueShopping }) => {
+  const cart = useSelector(state => state.cart.items);
+  const dispatch = useDispatch();
+
+  // Calculate total amount for all products in the cart
+  const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach((item) => {
+      const costNumber = parseFloat(item.cost.replace('$', ''));
+      total += costNumber * item.quantity;
+    });
+    return total.toFixed(2);
+  };
+
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+  };
+
+  const handleCheckoutShopping = () => {
+    alert('Coming Soon');
+  };
+
   return (
-    <div className="product-list">
-      <h2>Houseplants Category</h2>
-      <p>Add to Cart: <button>Add to Cart</button></p>
+    <div className="cart-container">
+      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <div>
+        {cart.map(item => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">Unit Price: {item.cost}</div>
+              <div className="cart-item-quantity">
+                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <span className="cart-item-quantity-value">{item.quantity}</span>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+              </div>
+              <div className="cart-item-total">Total: ${ (parseFloat(item.cost.replace('$', '')) * item.quantity).toFixed(2) }</div>
+              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="continue_shopping_btn">
+        <button className="get-started-button" onClick={onContinueShopping}>Continue Shopping</button>
+        <br />
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
+      </div>
     </div>
   );
-}
+};
 
-export default ProductList;
+export default CartItem;
